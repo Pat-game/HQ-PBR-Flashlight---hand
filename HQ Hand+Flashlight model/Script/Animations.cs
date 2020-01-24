ca12a55e98e581c5c814156050a38628a4c0fca8
Pat-game <33130837+Pat-game@users.noreply.github.com>
2020-01-24 09:07:11 +0100

    -

---
+++ HQ Hand+Flashlight model/Script/Animations.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+    private Animator anim;
+	public float WalkIdle;
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+
+            WalkIdle = 1;
+
+        }
+        else {
+
+            WalkIdle = 0;
+            
+        }
+
+        anim.SetFloat("Idle_walk",WalkIdle);
+	}
+
+    
+
+}
